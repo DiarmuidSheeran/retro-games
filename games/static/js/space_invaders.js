23443e18bcffe5f01eacb8ceb382db81f595dd63
Diarmuid Sheeran <dssheeran@outlook.com>
2024-03-12 20:04:34 +0000

    Create moveShooter to move squares into differetn divs removing them from current squares to add them into another div

--- games/static/js/space_invaders.js
+++ games/static/js/space_invaders.js
@@ -1,9 +1,12 @@
 const grid = document.querySelector('.grid')
 const resultDisplay = document.querySelector('.results')
 const width = 15
+const aliensRemoved = []
+let currentShooterIndex = 202
 
 for (let i = 0; i < width * width; i++) {
     const square = document.createElement('div')
+    square.id = i
     grid.appendChild(square)
 }
 
@@ -19,9 +22,40 @@ setTimeout(() => {
 
     function draw(){
         for (let i = 0; i < alienInvaders.length; i++){
-        squares[alienInvaders[i]].classList.add('invader')  
+            if (!aliensRemoved.includes(i)){
+                squares[alienInvaders[i]].classList.add('invader')
+            }  
         }
     }
     draw();
+
+    squares[currentShooterIndex].classList.add('shooter')
+
+    function moveShooter(e) {
+        squares[currentShooterIndex].classList.remove('shooter')
+        switch(e.key) {
+            case  'ArrowLeft':
+                if (currentShooterIndex % width !==0) currentShooterIndex -=1
+                break
+            case  'a':
+                if (currentShooterIndex % width !==0) currentShooterIndex -=1
+                break
+            case  'ArrowRight':
+                if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+                break
+            case  'd':
+                if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+                break
+        }
+        squares[currentShooterIndex].classList.add('shooter')
+    }
+
+    document.addEventListener('keydown', moveShooter)
+    
 }, 100);
+
+
+
+
+
 
