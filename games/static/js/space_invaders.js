a95d5af30046afa1a7e2f3dac040026d6a3d5c1c
Diarmuid Sheeran <dssheeran@outlook.com>
2024-03-14 00:05:24 +0000

    Update styling of background for space invaders

--- games/static/js/space_invaders.js
+++ games/static/js/space_invaders.js
@@ -95,7 +95,7 @@ function moveInvaders() {
         clearInterval(invadersId)
     }
 }
-invadersId = setInterval(moveInvaders, 600)
+invadersId = setInterval(moveInvaders, 200)
 
 function shoot(e) {
     let laserId
