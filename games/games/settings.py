504f43131f6c3cf069b92e7a48c6a4d7105bd872
Diarmuid Sheeran <dssheeran@outlook.com>
2024-03-20 19:17:46 +0000

    Update The allowed hosts in settings.py

--- games/games/settings.py
+++ games/games/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://diarmuid-retro-games-8c87fe8c80c2.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'diarmuid-retro-games-8c87fe8c80c2.herokuapp.com']
 
 
 # Application definition
@@ -85,16 +85,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+#DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
